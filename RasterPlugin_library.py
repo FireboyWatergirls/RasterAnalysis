5d358544f76e5f9d4a6185cf2def582cdb2b0ca7
WangZhiyee <609683213@qq.com>
2020-06-05 20:16:38 +0800

    计算GDP与亮度相关

--- RasterPlugin_library.py
+++ RasterPlugin_library.py
@@ -5,8 +5,47 @@ import os, sys
 from qgis.core import *
 from qgis.gui import *
 from PyQt5.QtCore import Qt, QFileInfo
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QFileDialog, QGridLayout, QWidget, QLabel
 from ui.mapView import Ui_MainWindow
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib
+matplotlib.use("Qt5Agg")  # 声明使用QT5
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class PlotLinear(QMainWindow):
+    def __init__(self):
+        super(PlotLinear, self).__init__()
+        self.figure1 = Figure()
+        self.canvas1 = FigureCanvas(self.figure1)
+        self.ax1 = self.figure1.add_subplot(111)
+
+        self.xishu_label = QLabel('相关系数：')
+
+        self.widget = QWidget()
+        self.setCentralWidget(self.widget)
+        self.statusBar().showMessage(' ')
+        self._main_layout = QGridLayout()
+        self.widget.setLayout(self._main_layout)
+        self._main_layout.addWidget(self.canvas1, 0, 0)
+        self._main_layout.addWidget(self.xishu_label, 1, 0, 1, -1)
+
+        self.setFixedSize(self.sizeHint())
+        self.show()
+
+    def plot(self, x, y, X, Y):
+        self.ax1.plot(x, y, 'k.')
+        self.ax1.plot(X, Y)
+        self.canvas1.draw()
+        x_temp = pd.DataFrame()
+        x_temp['x'] = [i[0] for i in x]
+        x_temp['y'] = [i[0] for i in y]
+        self.xishu_label.setText('相关系数：'+str(x_temp[u'x'].corr(x_temp[u'y'])))
 
 
 class MapExplorer(QMainWindow, Ui_MainWindow):
@@ -24,6 +63,47 @@ class MapExplorer(QMainWindow, Ui_MainWindow):
         self.actionpan.triggered.connect(self.action_pan_triggered)
         self.actionfull_extent.triggered.connect(self.action_fullextent_triggered)
         self.actionsave.triggered.connect(self.action_save_triggered)
+        self.toolButton.clicked.connect(self.open_gdp)
+        self.pushButton_2.clicked.connect(self.linear_regression)
+
+    def open_gdp(self):
+        fullpath, format = QFileDialog.getOpenFileNames(self, '打开数据', '',
+                                                       '*.csv')
+        gdp_data_temp = pd.read_csv(fullpath[0], encoding='gbk').to_dict(orient='index')
+        years = None
+        for i in gdp_data_temp[0].keys():
+            if i != '地区' and 'Unnamed' not in i:
+                years = i
+                break
+        if years is None:
+            print('数据有问题！')
+        else:
+            self.gdp_dict = {}
+            for i in range(31):
+                self.gdp_dict[gdp_data_temp[i]['地区']] = gdp_data_temp[i][years]
+
+            light_data_temp = pd.read_csv(fullpath[1], encoding='gbk').to_dict(orient='index')
+            self.light_dict = {}
+            for i in range(31):
+                self.light_dict[light_data_temp[i]['地区']] = light_data_temp[i]['亮度']
+
+    def linear_regression(self):
+        # self.light_dict和self.gdp_dict都是{城市名:值}
+        data = []
+        for k, v in self.gdp_dict.items():
+            data.append([v, self.light_dict[k]])
+        # data.sort()
+        x = []
+        y = []
+        for i in data:
+            x.append([i[0]])
+            y.append([i[1]])
+        reg = LinearRegression()
+        reg.fit(x, y)
+        X = list(range(int(min([i[0] for i in x])), int(max([i[0] for i in x]))))
+        Y = reg.predict(np.reshape(X, (-1, 1)))
+        self.plot_widget = PlotLinear()
+        self.plot_widget.plot(x, y, X, Y)
 
     def init_mapcanvas(self):
         #实例化地图画布
@@ -168,4 +248,6 @@ def main():
 
 
 if __name__ == '__main__':
+    import cgitb
+    cgitb.enable(format='text')
     main()
